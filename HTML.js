0d6acd553f15fdd16445fca78826f4de06dba404
mook0412 <72978053+mook0412@users.noreply.github.com>
2021-01-14 15:43:18 +0900

    Add files via upload

---
+++ HTML.js
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>smartphone</title>
+    <meta charset = "utf-8">
+</head>
+<body>
+    <h1>스마트폰 브랜드</h1>
+    <ol>
+        <li>apple</li>
+        <li>samsung</li>
+        <li>LG</li> 
+    </ol>    
+    <h2>개인적인 평가</h2>
+    <strong>삼성</strong>은 못생겼다<p><strong>아이폰</strong>은 이쁘기만하다</p><strong>엘지</strong>는 뭐지?
+</body>
+</html>
